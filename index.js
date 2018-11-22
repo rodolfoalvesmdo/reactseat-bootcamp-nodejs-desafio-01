da7700a2e8abe9bc6da07c6f5f764587f1914935
Rodolfo Alves <rodolfoalvesmdo@gmail.com>
2018-11-22 00:37:58 -0300

    Desafio conclído

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+app.set('view engine', 'njk')
+
+app.use(express.urlencoded({ extended: false }))
+
+const checkInput = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/')
+  }
+
+  return next()
+}
+
+app.get('/', (req, res) => {
+  return res.render('index')
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+  if (age >= 18) {
+    res.redirect(`/major?age=${age}`)
+  } else {
+    res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.get('/major', checkInput, (req, res) => {
+  const { age } = req.query
+  res.render('major', { age })
+})
+
+app.get('/minor', checkInput, (req, res) => {
+  const { age } = req.query
+  res.render('minor', { age })
+})
+
+app.listen(3000)
